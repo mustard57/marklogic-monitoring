43daa07f3ffb77f5c702af1cc3f498130094d48a
Ken Tune <ken.tune@marklogic.com>
2014-02-07 00:15:24 +0000

    delete geneos

--- server-script/marklogic-monitoring-for-geneos.sh
+++
@@ -1,7 +0,0 @@
-HOSTNAME=`hostname`
-PORT=8030
-SCRIPT_NAME=/app/procs/monitoring-output-for-geneos.xqy
-USER=dbTradeStore-Monitoring-user
-PASS=dbTradeStore-Monitoring-user
-
-curl -s --digest --user "$USER:$PASS" http://$HOSTNAME:${PORT}${SCRIPT_NAME}
